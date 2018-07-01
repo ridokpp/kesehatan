77bd401bf59de94f79c57e94be0cc31cf4050d2a
putrakanostra <40241543+putrakanostra@users.noreply.github.com>
2018-07-01 20:41:20 +0700

    rkm_medisnew

--- application/controllers/r_medis.php
+++ application/controllers/r_medis.php
@@ -73,6 +73,99 @@ class Account extends CI_Controller {
 
 	}
 
+
+	public function getkepala()
+	{
+		$dataCondition = array('kd_kepala' =>$this->input->post('kd_kepala'),
+							   'anemis_kiri' =>$this->input->post('anemis_kiri'),
+							   'anemis_kanan' =>$this->input->post('anemis_kanan'),
+							   'ikterik_kiri' =>$this->input->post('ikterik_kiri'),
+							   'ikterik_kanan' =>$this->input->post('ikterik_kanan'),
+							   'cianosis_kiri' =>$this->input->post('cianosis_kiri'),
+							   'cianosis_kanan' =>$this->input->post('cianosis_kanan'),
+							   'deformitas_kiri' =>$this->input->post('deformitas_kiri'),
+							   'deformitas_kanan' =>$this->input->post('deformitas_kanan'),
+							   'refchy_kiri' =>$this->input->post('refchy_kiri'),
+							   'refchy_kanan' =>$this->input->post('refchy_kanan'),
+							   'refchyopsi' =>$this->input->post('refchyopsi'));
+		$proses_insert = $this->Kesehatan_M->read('kepala',$dataCondition);
+		if ($proses_insert->num_rows() == 0) {
+			echo json_encode(array('status'=>'sukses'));
+		}else{
+			echo json_encode(array('status'=>'gagal'));
+		}
+
+	}
+
+	public function getthorak()
+	{
+		$dataCondition = array('kd_thorak' =>$this->input->post('kd_thorak'),
+							   'metris' =>$this->input->post('metris'),
+							   'wg_kiri' =>$this->input->post('wg_kiri'),
+							   'wg_kanan' =>$this->input->post('wg_kanan'),
+							   'rk_kiri' =>$this->input->post('rk_kiri'),
+							   'rk_kanan' =>$this->input->post('rk_kanan'),
+							   'vk_kiri' =>$this->input->post('vk_kiri'),
+							   'vk_kanan' =>$this->input->post('vk_kanan'),
+							   'jtgic' =>$this->input->post('jtgic'),
+							   's1_s2' =>$this->input->post('s1_s2'),
+							   's_tambahan' =>$this->input->post('s_tambahan'),
+							   'ket_tambahan' =>$this->input->post('ket_tambahan'));
+		$proses_insert = $this->Kesehatan_M->read('thorak',$dataCondition);
+		if ($proses_insert->num_rows() == 0) {
+			echo json_encode(array('status'=>'sukses'));
+		}else{
+			echo json_encode(array('status'=>'gagal'));
+		}
+
+	}
+
+	public function getabdomen()
+	{
+		$dataCondition = array('kd_abdomen' =>$this->input->post('kd_abdomen'),
+							   'nyeri_tekan' =>$this->input->post('nyeri_tekan'),
+							   'hpmgl' =>$this->input->post('hpmgl'),
+							   'spmgl' =>$this->input->post('spmgl'),
+							   'ket_tambahan' =>$this->input->post('ket_tambahan'));
+		$proses_insert = $this->Kesehatan_M->read('abdomen',$dataCondition);
+		if ($proses_insert->num_rows() == 0) {
+			echo json_encode(array('status'=>'sukses'));
+		}else{
+			echo json_encode(array('status'=>'gagal'));
+		}
+
+	}
+
+	public function getekstermitas()
+	{
+		$dataCondition = array('kd_ekstermitas' =>$this->input->post('kd_ekstermitas'),
+							   'ah' =>$this->input->post('ah'),
+							   'crt' =>$this->input->post('crt'),
+							   'edm' =>$this->input->post('edm'),
+							   'pitting' =>$this->input->post('pitting'),
+							   'ket_tambahan' =>$this->input->post('ket_tambahan'));
+		$proses_insert = $this->Kesehatan_M->read('ekstermitas',$dataCondition);
+		if ($proses_insert->num_rows() == 0) {
+			echo json_encode(array('status'=>'sukses'));
+		}else{
+			echo json_encode(array('status'=>'gagal'));
+		}
+
+	}
+
+	public function getterapi()
+	{
+		$dataCondition = array('kd_terapi' =>$this->input->post('kd_terapi'),
+							   'terapi' =>$this->input->post('terapi'));
+		$proses_insert = $this->Kesehatan_M->read('terapi',$dataCondition);
+		if ($proses_insert->num_rows() == 0) {
+			echo json_encode(array('status'=>'sukses'));
+		}else{
+			echo json_encode(array('status'=>'gagal'));
+		}
+
+	}
+
 	public function c_rkm()
 	{
 		$dataCondition = array('kd_rkm' =>$this->input->post('kd_rkm'),
