fce78e52fae2b9b14ef92801bb2683b963250b46
mochammadfadhli <mochammadfadhlizhil@gmail.com>
2018-07-21 19:48:38 +0700

    aldi

---
+++ application/views/petugas/antri.php
@@ -0,0 +1,78 @@
+<style type="text/css">
+    .tombol-proses{
+        height: 24px;
+        padding-top: 1px;
+    }
+</style>
+
+
+<h3 class="text-center mt-3">Antrian Pasien</h3>
+
+<div class="row">
+    <div class="col-8">
+        <h5 class="text-center mt-3">Daftar Pasien Terdaftar</h5>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">No.</th>
+                    <th scope="col">Nama</th>
+                    <th scope="col">Jam Datang</th>
+                    <th scope="col">Pembayaran</th>
+                    <th scope="col">Keterangan</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th>1</th>
+                    <td>Mark</td>
+                    <td>14:03:12</td>
+                    <td></td>
+                    <td><button class="btn btn-success btn-sm btn-block tombol-proses">Proses</button></td>
+                </tr>
+                <tr>
+                    <th>2</th>
+                    <td>Mark</td>
+                    <td>14:03:12</td>
+                    <td></td>
+                    <td><button class="btn btn-success btn-sm btn-block tombol-proses">Proses</button></td>
+                </tr>
+                <tr>
+                    <th>3</th>
+                    <td>Mark</td>
+                    <td>14:03:12</td>
+                    <td></td>
+                    <td><button class="btn btn-success btn-sm btn-block tombol-proses">Proses</button></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <div class="col-4">
+        <h5 class="text-center mt-3">Pasien Dalam Proses</h5>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">No.</th>
+                    <th scope="col">Nama</th>
+                    <th scope="col">Keterangan</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th>1</th>
+                    <td>Mark</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>2</th>
+                    <td>Mark</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>3</th>
+                    <td>Mark</td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>  
+</div>
