e78de12cdf6eeecce98a9147b894b42c02e418ce
romyazm <romyazzam19@gmail.com>
2018-08-01 23:42:27 +0700

    romy

--- application/views/dokter/log.php
+++ application/views/dokter/log.php
@@ -8,6 +8,14 @@ $(document).ready(function() {
     } );
 } );	
 </script>
+<style type="text/css">
+	.linone {
+		display: none;
+	}
+	.no-bullets {
+    list-style-type: none;
+	}
+</style>
 <h3 class="text mt-3" style="margin-left: 65px">Log Pemeriksaan Dokter</h3>
 <div class="container">
 	<div class="row mt-4">
@@ -82,31 +90,66 @@ $(document).ready(function() {
       <th>Planing</th>
     </tr>
   </thead>
-
   <tbody>
     <tr>
       <th>1</th>
-      <td>aku</td>
-      <td>dan kamu</td>
-      <td>kayak</td>
+      <td>
+      	<ul>
+      		<li class="no-bullets"> 10/12/2017 </li>
+      		<li class="no-bullets"> 19:00 WIB </li>
+      	</ul>
+      </td>
+      <td>Batuk</td>
+      <td>
+      	<ul>
+      		<li class="no-bullets">TB/BB : 70 cm/ 46 Kg</li>
+   			<li class="no-bullets">TD : ..../.... mmHg RR</li>
+      		<li class="no-bullets">N  : ....rpm TAx: 36c</li>
+      		<li class="no-bullets">Head to Toe :</li>
+      	</ul>	 
+      </td>
       <td>asu</td>
-      <td>loo</td>
+      <td>R/</td>
     </tr>
     <tr>
       <th>2</th>
-       <td>oke</td>
-      <td>masuk</td>
-      <td>bos</td>
+       <td>
+       	<ul>
+      		<li class="no-bullets"> 10/12/2017 </li>
+      		<li class="no-bullets"> 19:00 WIB </li>
+      	</ul>
+       </td>
+      <td>Pilek</td>
+      <td>
+      	<ul>
+      		<li class="no-bullets">TB/BB : 70 cm/ 46 Kg</li>
+   			<li class="no-bullets">TD : .... /.... mmHg RR</li>
+      		<li class="no-bullets">N  : .... rpm TAx: 36c</li>
+      		<li class="no-bullets">Head to Toe :</li>
+      	</ul>	 
+      </td>
       <td>raimu</td>
-      <td>meduk</td>
+      <td>R/</td>
     </tr>
     <tr>
       <th>3</th>
-       <td>Mangan</td>
-      <td>Pindang</td>
-      <td>Lawuh</td>
+       <td>
+       	<ul>
+      		<li class="no-bullets"> 10/12/2017 </li>
+      		<li class="no-bullets"> 19:00 WIB </li>
+      	</ul>
+       </td>
+      <td>Pusing</td>
+      <td>
+      	<ul>
+      		<li class="no-bullets">TB/BB : 70 cm/ 46 Kg</li>
+   			<li class="no-bullets">TD : ..../.... mmHg RR</li>
+      		<li class="no-bullets">N  : ....rpm TAx: 36c</li>
+      		<li class="no-bullets">Head to Toe :</li>
+      	</ul>	 
+      </td>
       <td>krepek</td>
-      <td>tempe</td>
+      <td>R/</td>
     </tr>
   </tbody>
 </table>
