fa306a3b133d9c74f714847909d12f257d3126cc
putrakanostra <40241543+putrakanostra@users.noreply.github.com>
2018-08-15 23:55:29 +0700

    ddbb

---
+++ application/views/dokter/suratrujukan.php
@@ -0,0 +1,238 @@
+<br>
+
+<div class="container">
+	<div class="row">
+		<div class="col">
+			<div class="row">
+				<div class="col-2 offset-1 "> 
+					<img class="img-responsive" src="<?=base_url()?>assets/images/LOGO YAYASAN.png" style="width:150px">
+				</div>
+				<div class="col-8">
+					<div>
+						<p class="text-success text-center mb-0 font-weight-bold">YAYASAN DARUL'ULUM AGUNG</p>
+					</div>
+					<div>
+						<p class="text-center mb-3 font-weight-bold">PRAKTIK DOKTER UMUM</p>
+					</div>
+					<div>
+						<p class="text-center mb-0 font-weight-bold">Jl. Mayjen Sungkono No 09 Bumiayu Kedungkandang Malang 65135</p>
+					</div>
+					<div>
+						<p class="text-center mb-0 font-weight-bold">Telp. 0341-752866, Fax. 0341-752</p>
+					</div>
+					<div>
+						<p class="text-center mb-0 font-weight-bold">Akte Notaris : H. Romlan, SH, M.Hum. No. 26 Tanggal 19 November 2015</p>
+					</div>
+				</div>
+			</div>
+			<hr class="mt-0 mb-0">
+				<div class="row mb-4">
+				Kepada YTH.
+				</div>
+			
+			<div class="row">
+				<div class="col-1">
+				Perihal
+				</div>
+				<div class="col-0">
+				:
+				</div>
+				<div class="col-9">
+				Rujukan Pasien
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-1">
+				No. Surat
+				</div>
+				<div class="col-0">
+				:
+				</div>
+				<div class="col-9">
+				....../003/....../2018
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-1">
+				Lampiran
+				</div>
+				<div class="col-0">
+				:
+				</div>
+				<div class="col-9">
+				...................................
+				</div>
+			</div>
+			<div class="row mb-4">
+				Assalamu'allaikum Wr. Wb.<br>
+				Mohon konsul dan penatalaksanaan lebih lanjut pada penderita;
+			</div>
+			<div class="row">
+				<div class="col-2"> 
+					Nama
+				</div>
+				<div class="col-0">
+					:
+				</div>	
+				<div class="col-9">
+					..............................................................................................
+				</div>		
+			</div>
+			<div class="row">
+				<div class="col-2"> 
+					NIK
+				</div>
+				<div class="col-0">
+					:
+				</div>	
+				<div class="col-9">
+					..............................................................................................
+				</div>		
+			</div>
+			<div class="row">
+				<div class="col-2"> 
+					Tempat / Tgl Lahir
+				</div>
+				<div class="col-0">
+					:
+				</div>
+				<div class="col-9">
+					........................./...................................................................
+				</div>	
+			</div>
+			<div class="row">
+				<div class="col-2"> 
+					Jenis Kelamin		
+				</div>
+				<div class="col-0">
+					:
+				</div>
+				<div class="col-9">
+					Laki - Laki / Perempuan * )
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-2"> 	
+					Pekerjaan					
+				</div>
+				<div class="col-0">
+					:
+				</div>
+				<div class="col-9">
+					...............................................................................................
+				</div>
+			</div>
+			<div class="row mb-4">
+				<div class="col-2"> 
+					Alamat		
+				</div>
+				<div class="col-0">
+					:
+				</div>
+				<div class="col-9">
+					...............................................................................................
+				</div>
+			</div>
+			<div class="row mb-4">
+				Hasil Pemeriksaan
+				<div class="col-2">
+					:
+				</div>
+			<div class="row">
+				<div class="col-2"> 
+					Keluhan
+				</div>
+				<div class="col-0">
+					:
+				</div>	
+				<div class="col-9">
+					..............................................................................................
+				</div>		
+			</div>
+			<div class="row">
+				<div class="col-2"> 
+					GCS
+				</div>
+				<div class="col-0">
+					:
+				</div>	
+				<div class="col-9">
+					E ...  V ...  M ......................................................................................
+				</div>		
+			</div>
+			<div class="row">
+				<div class="col-2"> 
+					Tempat / Tgl Lahir
+				</div>
+				<div class="col-0">
+					:
+				</div>
+				<div class="col-9">
+					........................./...................................................................
+				</div>	
+			</div>
+			<div class="row">
+				<div class="col-2"> 
+					Jenis Kelamin		
+				</div>
+				<div class="col-0">
+					:
+				</div>
+				<div class="col-9">
+					Laki - Laki / Perempuan * )
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-2"> 	
+					Pekerjaan					
+				</div>
+				<div class="col-0">
+					:
+				</div>
+				<div class="col-9">
+					...............................................................................................
+				</div>
+			</div>
+			<div class="row mb-4">
+				<div class="col-2"> 
+					Alamat		
+				</div>
+				<div class="col-0">
+					:
+				</div>
+				<div class="col-9">
+					...............................................................................................
+				</div>
+			</div>
+
+			</div>
+			<div class="row">
+				<div class="col mt-4">
+					<div class="row"  style="margin-top: 100px">
+						NB : .*) Coret yang tidak perlu.
+					</div>
+					<div class="row">
+						**Surat ini tidak sah jika tidak terdapat cap stampel.
+					</div>
+					<div class="row">
+						Istirahat sakit diberikan maksimal selama 3 (tiga) hari.
+					</div>
+				</div>
+				<div class="col-4">
+					<div class="row">
+						Malang, ........................... 2018
+					</div>
+					<div class="row" style="margin-bottom: 100px">
+						Pemeriksa,
+					</div>
+					<div class="row mt-5">
+						dr. Muchamad Zubaid
+					</div>
+					<div class="row">
+						SIP : 446.DU / 1616.1 / 35.73.302 / 2018
+					</div>
+				</div>	
+			</div>
+		</div>	
+	</div>
+</div>
